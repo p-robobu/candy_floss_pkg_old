ca5c83e7bf085e95cc5d8019b993569e7e5146d6
Kenta Kubota <zingl.0523@gmail.com>
2019-11-22 03:39:44 +0900

    added pwm fuction

--- script/test_rpi3_gpio.py
+++ script/test_rpi3_gpio.py
@@ -15,6 +15,15 @@ class GpioCtrl:
         rospy.loginfo('GPIO' + str(self.pinNo) + ' ON')
         GPIO.output(self.pinNo, True)
 
+
+    def gpio_pwm_on(self, duty):
+        rospy.loginfo('GPIO' + str(self.pinNo) + ' PWM Duty ' + str(duty) + '[%] ON')
+        while not rospy.is_shutdown():
+            GPIO.output(self.pinNo, True)
+            time.sleep(duty/10000.0)
+            GPIO.output(self.pinNo, False)
+            time.sleep(0.01 - (duty/10000.0))
+
     def gpio_off(self):
         rospy.loginfo('GPIO' + str(self.pinNo) + ' OFF')
         GPIO.output(self.pinNo, False)
@@ -24,8 +33,13 @@ class GpioCtrl:
         rospy.loginfo('GPIO cleanup done')
 
 if __name__ == '__main__':
-    gpioCtrl = GpioCtrl(2)
-    gpioCtrl.gpio_on()
-    time.sleep(5)
+    gpioCtrl = GpioCtrl(26)
+    gpioCtrl.gpio_pwm_on(10)
     gpioCtrl.gpio_off()
     gpioCtrl.gpio_cleanup()
+
+
+    #gpioCtrl.gpio_on()
+    #time.sleep(5)
+    #gpioCtrl.gpio_off()
+    #gpioCtrl.gpio_cleanup()
